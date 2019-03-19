d14a1f16de4d6e9780687172ffb1ea352d62fdcf
Chris Newland <chris@chrisnewland.com>
2019-03-19 21:28:14 +0000

    GraalVM options updated to RC14

--- dumpGraalOptions.sh
+++ dumpGraalOptions.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-../graalvm-ce-1.0.0-rc13/bin/java -XX:+JVMCIPrintProperties > graal_ce.out
-../graalvm-ee-1.0.0-rc13/bin/java -XX:+JVMCIPrintProperties > graal_ee.out
+../graalvm-ce-1.0.0-rc14/bin/java -XX:+JVMCIPrintProperties > graal_ce.out
+../graalvm-ee-1.0.0-rc14/bin/java -XX:+JVMCIPrintProperties > graal_ee.out
 
