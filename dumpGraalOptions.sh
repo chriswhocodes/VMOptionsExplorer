0ffef66d7611b55737abe8b8d5b8ad339c0c4b44
Chris Newland <chris@chrisnewland.com>
2019-04-06 15:27:15 +0100

    updated for GraalVM rc15

--- dumpGraalOptions.sh
+++ dumpGraalOptions.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-../graalvm-ce-1.0.0-rc14/bin/java -XX:+JVMCIPrintProperties > graal_ce.out
-../graalvm-ee-1.0.0-rc14/bin/java -XX:+JVMCIPrintProperties > graal_ee.out
+../graalvm-ce-1.0.0-rc15/bin/java -XX:+JVMCIPrintProperties > graal_ce.out
+../graalvm-ee-1.0.0-rc15/bin/java -XX:+JVMCIPrintProperties > graal_ee.out
 
