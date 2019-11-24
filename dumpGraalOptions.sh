11e63ddc9b6ba3a91b7ae19110441a5336fb4d8e
Chris Newland <chris@chrisnewland.com>
2019-11-24 22:39:25 +0000

    GraalVM options dumps from JDK8 and JDK11 VM and native-image

--- dumpGraalOptions.sh
+++ dumpGraalOptions.sh
@@ -4,12 +4,31 @@
 # install EE native image with gu install --file native-image-installable-svm-svmee-linux-amd64-19.2.0.1.jar
 # downloaded from https://www.oracle.com/technetwork/graalvm/downloads/index.html
 
-export GRAAL_BIN_CE=/home/chris/openjdk/graalvm-ce-19.2.1/bin
-export GRAAL_BIN_EE=/home/chris/openjdk/graalvm-ee-19.2.1/bin
+export GRAAL_VERSION=19.3.0
 
-$GRAAL_BIN_CE/java -XX:+JVMCIPrintProperties > graal_ce.vm 2>/dev/null
-$GRAAL_BIN_EE/java -XX:+JVMCIPrintProperties > graal_ee.vm 2>/dev/null
+# JAVA 8
 
-$GRAAL_BIN_CE/native-image --expert-options-all > graal_ce.native 2>/dev/null
-$GRAAL_BIN_EE/native-image --expert-options-all > graal_ee.native 2>/dev/null
+export JAVA_VERSION=java8
+
+export GRAAL_BIN_CE_8=/home/chris/openjdk/graalvm-ce-$JAVA_VERSION-$GRAAL_VERSION/bin
+export GRAAL_BIN_EE_8=/home/chris/openjdk/graalvm-ee-$JAVA_VERSION-$GRAAL_VERSION/bin
+
+$GRAAL_BIN_CE_8/java -XX:+JVMCIPrintProperties > graal_ce_$JAVA_VERSION.vm 2>/dev/null
+$GRAAL_BIN_EE_8/java -XX:+JVMCIPrintProperties > graal_ee_$JAVA_VERSION.vm 2>/dev/null
+
+$GRAAL_BIN_CE_8/native-image --expert-options-all > graal_ce_$JAVA_VERSION.native 2>/dev/null
+$GRAAL_BIN_EE_8/native-image --expert-options-all > graal_ee_$JAVA_VERSION.native 2>/dev/null
+
+# JAVA 11
+
+export JAVA_VERSION=java11
+
+export GRAAL_BIN_CE_11=/home/chris/openjdk/graalvm-ce-$JAVA_VERSION-$GRAAL_VERSION/bin
+export GRAAL_BIN_EE_11=/home/chris/openjdk/graalvm-ee-$JAVA_VERSION-$GRAAL_VERSION/bin
+
+$GRAAL_BIN_CE_11/java -XX:+JVMCIPrintProperties > graal_ce_$JAVA_VERSION.vm 2>/dev/null
+$GRAAL_BIN_EE_11/java -XX:+JVMCIPrintProperties > graal_ee_$JAVA_VERSION.vm 2>/dev/null
+
+$GRAAL_BIN_CE_11/native-image --expert-options-all > graal_ce_$JAVA_VERSION.native 2>/dev/null
+$GRAAL_BIN_EE_11/native-image --expert-options-all > graal_ee_$JAVA_VERSION.native 2>/dev/null
 
